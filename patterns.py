edc5af05a6c0c3850e2d56bbf808cc6e7a2a6ca4
tiff178 <tiffmt817@gmail.com>
2024-11-16 12:14:47 -0800

    functions for apriori algo

--- patterns.py
+++ patterns.py
@@ -7,17 +7,35 @@ def apriori(itemsets, threshold):
     # calculate the minimum support count 
     min_support = (threshold / 100) * len(itemsets)
 
-    # calculate min support
-    # find frequent 1-itemsets
-    # iterative candidate generation
-    # filter frequent itemsets
-    # repeat 
-    # return results
-
+    # implement the function here (?)
+    
     # Should return a list of pairs, where each pair consists of the frequent itemset and its support 
     # e.g. [(set(items), 0.7), (set(otheritems), 0.74), ...]
     return []
 
+# function to generate candidate k-itemsets (k >= 2)
+def apriori_gen(Lk_1, k):
+    Ck = set() # set to store candidate k-itemsets (Ck)
+
+    # compare every pair of (k-1) itemsets in Lk-1 to generate candidate k-itemsets
+    for i in range(len(Lk_1)):
+        for j in range(i+1, len(Lk_1)):
+            l1, l2 = Lk_1[i][0], Lk_1[j][0] 
+
+            if list(l1)[:k-2] == list(l2)[:k-2]:
+                c = l1.union(l2) # join two itemsets to form a candidate (c)
+
+                if not has_infrequent_subset(c, Lk_1):
+                    Ck.add(c)
+    return Ck
+
+# function to check if candidate k-itemset has any infrequent (k-1) itemsets
+def has_infrequent_subset(c, Lk_1):
+    for i in range(len(c)):
+        subset = frozenset(c).difference([c[i]])
+        if not any(subset == itemset[0] for itemset in Lk_1):
+            return True
+    return False
     
 # DO NOT CHANGE THE FOLLOWING LINE
 def association_rules(itemsets, frequent_itemsets, metric, metric_threshold):
